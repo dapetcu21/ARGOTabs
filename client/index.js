7ad19cefaa78eb260e4c16d732a8081c8509404a
Marius Petcu <dapetcu21@gmail.com>
2016-07-18 12:17:09 +0300

    Fix modal top position issue

--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
 require('./core/css/index.styl');
 require('file?name=favicon.ico!./core/assets/favicon.ico');
 
-const UIController = require('./core/uicontroller')
 const templateIndex = require('./core/templates/index.jade');
 
 const body = document.body;
 body.innerHTML = templateIndex() + body.innerHTML;
 
+const UIController = require('./core/uicontroller')
 window.ARGOTabs = {};
 window.ARGOTabs.uiController = new UIController();
