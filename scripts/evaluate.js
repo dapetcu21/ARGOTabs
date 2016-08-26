c97167427a417539ea33f90696ab9f346da03d6a
Marius Petcu <dapetcu21@gmail.com>
2016-08-26 11:25:39 +0300

    Add judge evaluation script

---
+++ scripts/evaluate.js
@@ -0,0 +1,30 @@
+function evaluate () {
+  let t = window.ARGOTabs.uiController.tournament
+  let judges = t.judges
+  let rows = judges.map(judge => ({ judge, rounds: [] }))
+  let rounds = t.rounds
+
+  function evaluateRound (round) {
+    rows.forEach(row => {
+      const stats = row.judge.rounds[round.id]
+      let ret = stats.ballot
+      ? { teams: stats.ballot.teams.map(team => team.name), status: stats.shadow ? 'shadow' : 'chair' }
+      : { teams: ['', ''], status: 'idle' }
+      row.rounds.push(ret)
+    })
+  };
+
+  rounds.forEach(evaluateRound)
+
+  return rows.map(row => {
+    let map = {}
+    row.rounds.forEach(ret => ret.teams.forEach(team => {
+      if (team && !map[team]) { map[team] = 0 }
+      if (team) { map[team]++ }
+    }))
+    return row.rounds.reduce((acc, ret) =>
+      acc + ',' + ret.status + ',' + ret.teams.map(team => map[team] ? team + ' (' + map[team] + ')' : team).join(','),
+      row.judge.name
+    )
+  }).join('\n')
+}
