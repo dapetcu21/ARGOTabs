6a83c9fcae8b0a28c31ce45c36e5a2a01bd168d1
Marius Petcu <dapetcu21@gmail.com>
2017-04-11 04:02:37 +0300

    Add history fallback to production build

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 var express = require('express')
 var path = require('path')
+var fallback = require('express-history-api-fallback')
 var app = express()
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+var root = path.join(__dirname, '..', 'public')
+app.use(express.static(root))
+app.use(fallback('index.html', { root }))
 app.listen(process.env.PORT || 3000)
