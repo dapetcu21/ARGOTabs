3c76f999b6b49b705076d8abb0d893201f8e1d9e
Marius Petcu <dapetcu21@gmail.com>
2017-04-19 02:06:54 +0300

    Return 404 for app cache manifest

--- server/index.js
+++ server/index.js
@@ -6,5 +6,8 @@ var app = express()
 
 var root = path.join(__dirname, '..', 'public')
 app.use(express.static(root))
+app.get('/manifest.appcache', (req, res) => {
+  res.status(404).send('App Cache manifest is currently disabled')
+})
 app.use(fallback('index.html', { root }))
 app.listen(process.env.PORT || 3000)
